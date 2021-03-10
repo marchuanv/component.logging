78023452bf55f5e58b436655173fbaba75601bf0
marchuanv <marchuanv@gmail.com>
2021-03-10 12:24:49 +0200

    refactor

--- test.js
+++ test.js
@@ -1,13 +1,10 @@
 const Component = require("component");
 (async()=>{
-    const componentLogging = require("./component.logging.js");
     const component = new Component("TEST")
     const { componentRequestHandler } = await component.require("component.request.handler", { gitUsername: "marchuanv" });
     if (!componentRequestHandler){
         throw "Test Failed";
     }
-    componentLogging.write("component.logging","something happened 1");
-    componentLogging.write("component.logging","something happened 2");
 })().catch((err)=>{
     console.error(err);
 });
